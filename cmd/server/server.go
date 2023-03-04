b0883aeae6f13aaaef0b71d32fb2b2bbbaf885dc
Guilherme A <guilherme.almeida@before.com.br>
2023-03-04 09:43:12 -0300

    add category

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"gqlgenProj/graph"
 	"log"
 	"net/http"
@@ -8,17 +9,32 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"gqlgenProj/internal/database"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const defaultPort = "8080"
 
 func main() {
+
+	db, err := sql.Open("mysql", "root:root@tcp(172.24.0.3:3306)/teste")
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+	//courseDb := database.NewCourse(db)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDb,
+		//CourseDB:   courseDb,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
